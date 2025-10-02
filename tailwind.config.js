ab4300208dcb1f6b002f86a480a51ce0c46592a4
Marcos Iriarte Bosnic <iriarte.marcos@gmail.com>
2025-10-02 10:26:53 -0300

    remove tailwind

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
-  darkMode: false, // Completely disable dark mode
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
